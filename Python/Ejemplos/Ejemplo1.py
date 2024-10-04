d17deb25cd188142cec27ff14bb724742c6c1d70
mpinillaa <165885567+mpinillaa@users.noreply.github.com>
2024-10-04 10:37:00 +0200

    Add files via upload

---
+++ Python/Ejemplos/Ejemplo1.py
@@ -0,0 +1,39 @@
+"""nombre="Rosa"
+print(type(nombre))
+nombre="marcos"
+print(type(nombre)) # escribir en minuscula, si te equivocas en una variable te la cambia a una sola
+letra=nombre[0]
+print(letra)
+dni="11111111A"
+letra=dni[-1]
+print(letra)
+codigo="28034"
+ciudad=codigo[0:2]
+print(ciudad)
+poblacion=codigo[2:3]
+print(poblacion)
+barrio=codigo[:-2]  #280
+print(barrio)
+numero=int("34.4")
+print(numero)
+x=3
+x+=1 """
+
+a=5
+b=5
+print(id(a))
+print(id(b))
+b=a+1
+c=6
+print(id(a))
+print(id(b))
+print(id(c))
+
+
+
+#funcial all() y any()
+#in = te devuekve true o false
+#del a = elimina la variable
+#list, tuple, range = tipos de datos
+#dicicionarios
+#cadenas(clase str = 'python)
