542601894657c1898700c698afd335a204e4b53d
mpinillaa <165885567+mpinillaa@users.noreply.github.com>
2024-10-08 14:37:35 +0200

    Add files via upload

---
+++ Python/Practica2/Ej1.py
@@ -0,0 +1,5 @@
+retirada=float(input("cuanto quieres sacar:"))
+
+multiplicador = (retirada // 1000) + 1  
+
+print("comision:",10*multiplicador,"euros")
---
+++ Python/Practica2/ej3.py
@@ -0,0 +1,13 @@
+
+caracter = input("Anota un caracter  ")
+    
+if len(caracter) ==1:
+    if caracter >="a" and caracter <="z":   #isnumeric()
+        print("es minusula")
+    elif caracter >="A" and caracter <="Z": #upper()
+        print("es mayuscula")
+    elif caracter >="0" and caracter <="9": #
+        print("es un numero")
+        
+else:
+    print("escribe solo una letra")
