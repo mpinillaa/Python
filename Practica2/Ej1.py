38c1c6099938d2dbefdb94419338684733d25bad
mpinillaa <165885567+mpinillaa@users.noreply.github.com>
2024-11-10 18:32:05 +0100

    Delete Practica2 directory

--- Practica2/Ej1.py
+++
@@ -1,5 +0,0 @@
-retirada=float(input("cuanto quieres sacar:"))
-
-multiplicador = (retirada // 1000) + 1  
-
-print("comision:",10*multiplicador,"euros")
--- Practica2/Ej2.py
+++
@@ -1,8 +0,0 @@
-
-mes=int(input("numero a sacar del 1 al 12:  "))
-while mes<=0 | mes>=12:
-    mes= int(input("  Debes intriducir un mes valido del 1 al 12  "))
-    
-    
-    
-    
