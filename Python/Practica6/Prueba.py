c213b86c37e22847297b78a7dbd181480a381526
mpinillaa <165885567+mpinillaa@users.noreply.github.com>
2024-11-13 14:32:56 +0100

    Add files via upload

--- Python/Practica6/Prueba.py
+++ Python/Practica6/Prueba.py
@@ -1,11 +1,23 @@
-from validar.Usuarios import validarUsu
-from validar.Contraseña import validarContra
+from Validar.Ej1.Usuarios import validarUsu
+from Validar.Ej1.Contraseña import validarContra
 
-usuarios="Albertostas*"
-validar=validarUsu(usuarios)
-print(validar)
+usuarios=input("introduce el usuario, introduce '*' para salir:   ")
+contraseña=input("introduce la contraseña del usuario:   ")
+dicUsuarios={}
 
+validar=True  
+"""no es necesario creo"""
+while usuarios != "*":
+    validar= validarUsu(usuarios)
+    print(validar)
+    if validar==True:
+        validar=validarContra(contraseña)
+        if validar==True:
+            dicUsuarios[usuarios]=contraseña
+            print("la contraseña se ha creado correctamente")
+            
+    usuarios=input("introduce el usuario, introduce '*' para salir:   ")
+    contraseña=input("introduce la contraseña del usuario:   ")     
+    
+print(dicUsuarios)
 
-contraseña="CAngre*jo23*"
-validar= validarContra(contraseña)
-print(validar)
